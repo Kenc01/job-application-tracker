189db3bca7f074b84668e7b1b9acad1e560f965e
Kenc01 <keithbrianlaranjo82@gmail.com>
2026-02-06 23:11:50 +0800

    Add sign-up page, auth setup, UI components, and database configuration

--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -1,3 +1,60 @@
+"use client";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardContent,
+  CardFooter,
+} from "@/components/ui/card";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+
 export default function SignUp() {
-  return <div>Sign Up Page</div>;
+  return (
+    <div className="flex min-h-[calc(100vh-4rem)] items-center justify-center bg-white p-4 ">
+      <Card>
+        <CardHeader>
+          <CardTitle className="">Sign Up</CardTitle>
+          <CardDescription className="">
+            Create an account to start tracking your job applications.
+          </CardDescription>
+        </CardHeader>
+        <form>
+          <CardContent>
+            <div>
+              <Label htmlFor="name">Name</Label>
+              <Input
+                id="name"
+                type="text"
+                placeholder="Keith Laranjo"
+                required
+              />
+            </div>
+
+            <div>
+              <Label htmlFor="email">Email</Label>
+              <Input
+                id="email"
+                type="email"
+                placeholder="keith@example.com"
+                required
+              />
+            </div>
+
+            <div>
+              <Label htmlFor="password">Name</Label>
+              <Input
+                id="password"
+                type="password"
+                placeholder="Keith Laranjo"
+                required
+              />
+            </div>
+          </CardContent>
+          <CardFooter></CardFooter>
+        </form>
+      </Card>
+    </div>
+  );
 }
